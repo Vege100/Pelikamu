297d7373bd64ccef230d93db53ea8da7a8b6c7aa
Kilpeläinen Verneri Valo Johannes <verneri.k.kilpelainen@student.jyu.fi>
2023-02-01 14:51:36 +0200

    Kansio

---
+++ Pelikamu/src/kayttoliittyma/PaaValikkoController.java
@@ -0,0 +1,10 @@
+package kayttoliittyma;
+
+/**
+ * @author Verneri
+ * @version 1.2.2023
+ *
+ */
+public class PaaValikkoController {
+      // TODO
+}
---
+++ Pelikamu/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Verneri
+ *
+ */
+module Pelikamu {
+}
