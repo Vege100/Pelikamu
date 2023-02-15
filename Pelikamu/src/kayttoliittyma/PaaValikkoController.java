d9c293ed675545f6d391fe94930f74ad74108df5
Kilpeläinen Verneri Valo Johannes <verneri.k.kilpelainen@student.jyu.fi>
2023-02-15 12:50:23 +0200

    Hahmosivu lisätty

--- Pelikamu/src/kayttoliittyma/PaaValikkoController.java
+++ Pelikamu/src/kayttoliittyma/PaaValikkoController.java
@@ -1,9 +1,16 @@
 package kayttoliittyma;
 
+import java.io.IOException;
+
 import fi.jyu.mit.fxgui.Dialogs;
 import fi.jyu.mit.fxgui.ModalController;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -12,11 +19,37 @@ import javafx.stage.Stage;
  *
  */
 public class PaaValikkoController {
+    @FXML
+    private Pane Hahmosivu;
+    
+    @FXML
+    private Pane rootpane;
 
     @FXML
-    void LisaaPeli(ActionEvent event) {
+    void handleHahmot(ActionEvent event) throws IOException {
+        Pane pane = FXMLLoader.load(getClass().getResource("Hahmot.fxml"));
+        rootpane.getChildren().setAll(pane);
+    }
+    @FXML
+    void handleLisääPeli(ActionEvent event) {
         ModalController.showModal(PaaValikkoController.class.getResource("PeliLisäys.fxml"), "Jäsen", null, "");
     }
+
+    @FXML
+    void handleLopeta(ActionEvent event) {
+            Platform.exit();
+    }
+
+    @FXML
+    void handlePoistaPeli(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTulosta(ActionEvent event) {
+
+    }
+
 
     @FXML
     void Teemat(ActionEvent event) {
@@ -28,4 +61,6 @@ public class PaaValikkoController {
         ModalController.showModal(PaaValikkoController.class.getResource("Aloitusnäyttö.fxml"), "Jäsen", null, "");
     }
 
+    // --------------------------------------------------------------------------
+    
 }
