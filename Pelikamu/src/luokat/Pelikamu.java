85d5430c594fbd8496acd79357fd687f5a1cffc7
Kilpeläinen Verneri Valo Johannes <verneri.k.kilpelainen@student.jyu.fi>
2023-03-06 21:56:26 +0200

    tehty luokat

---
+++ Pelikamu/src/luokat/Pelikamu.java
@@ -0,0 +1,56 @@
+package luokat;
+
+public class Pelikamu {
+        private final Hahmot hahmot = new Hahmot();
+        private final Pelit pelit = new Pelit();
+        
+        
+        /**
+         * Palauttaa pelien määrän
+         * @return pelien määrä kokonaislukuna
+         */
+        public int getPelit() {
+            return pelit.getLkm();
+        }
+        /**
+         * Lisää uuden pelin
+         * @param peli joka lisätään
+         */
+        public void add(Peli peli) {
+            pelit.add(peli);
+        }
+        /**
+         * Lisää uuden hahmon
+         * @param hahmo joka lisätään
+         */
+        public void add(Hahmo hahmo) {
+            // TODO mahdollisesti toista kautta, sillä hahmoja lisätään harvoin ja tarvitsevat ehkä kuvan..
+            hahmot.add(hahmo);
+        }
+        
+        public Peli getPeli(int i) {
+            return pelit.get(i);
+        }
+        
+        public List<Peli> givePelit(Hahmo hahmo) {
+            return Pelit.annaPelit(hahmo.getId());
+        }
+        
+        public void readFile(String nimi) {
+            pelit.readFile();
+            hahmot.readFile();
+        }
+        
+        public void save() {
+            hahmot.save();
+            pelit.save();
+        }
+        
+        //public static void main(String args[]) {
+        // TODO mahdolliset testit ja alustus asiakasta varten    
+        //}
+        
+        
+        
+        
+}
---
+++ Pelikamu/src/luokat/Pelit.java
@@ -0,0 +1,36 @@
+package luokat;
+/**
+ *  Hallitsee pelejä
+ * @author Verneri
+ * @version 6 Mar 2023
+ *
+ */
+public class Pelit {
+    private static final int    MAX_PELEJA      = 5;
+    private int                 lkm             = 0;
+    private String              tiedostonNimi   = "";
+    private Peli[]              alkiot          = new Peli[MAX_PELEJA];
+
+    
+    /**
+     * Oletusmuodostaja
+     */
+    public Pelit() {
+        
+    }
+    
+    public void add(Peli peli) {
+        alkiot[lkm] = peli;
+        lkm++;
+    }
+    
+    public Peli get(int i) {
+        return alkiot[i];
+    }
+    
+    public void readFile()
+    
+    
+}
+
+
