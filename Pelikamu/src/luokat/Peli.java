9c24b70b300f01ac8767a198f198d3363a617413
Kilpeläinen Verneri Valo Johannes <verneri.k.kilpelainen@student.jyu.fi>
2023-03-07 20:14:12 +0200

    Luokkia

--- Pelikamu/src/luokat/Peli.java
+++ Pelikamu/src/luokat/Peli.java
@@ -1,5 +1,36 @@
 package luokat;
 
+import 
+
 public class Peli {
+        private int id;
+        private int hId;
+        private boolean win = false;
+        private int kills = 0;
+        private int deaths = 0;
+        private int assists = 0;
+        private int timeM;
+        private int timeS;
+        private String gameStyle;
+        private int idCount = 1;
+        
+        
+        public int register() {
+            id = idCount;
+            idCount++;
+            return id;
+        }
+        
+        public void parse(String )
+        
+        public void print(PrintStream out) {
+            out.println(String.format("%02d/%02d%%02d",kills,deaths,assists) + timeM + ":" +  timeS +" " + Hahmot.champions);
+        }
+        
+        
+        
+        
+        
+        
 
 }
